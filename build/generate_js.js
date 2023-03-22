eceae34fa7f263b54e7a6f78c93e27fa9829ac39
Kai Werther <kaiwerther@gmail.com>
2023-03-22 19:00:06 +0100

    changed build script to add the icon registry

--- build/generate_js.js
+++ build/generate_js.js
@@ -7,7 +7,6 @@ const fs = require('fs').promises;
 const fsr = require('fs');
 const path = require('path');
 
-const regex = /<svg.*?width="(.*?)".*?height="(.*?)".*?fill="(.*?)".*?viewBox="(.*?)".*?>(.*)<\/svg>/m;
 const svgSourceFolder = './svg/';
 const jsTargetFolder = './generated_js/';
 
@@ -37,57 +36,33 @@ fs.readdir(svgSourceFolder).then(async (files) => {
   for (const file of files) {
     // Make one pass and make the file complete
     const svgFile = path.join(svgSourceFolder, file);
-    const jsFile = path.join(jsTargetFolder, file.replace('.svg', '.js'));
 
     const stat = await fs.stat(svgFile);
 
     if (stat.isFile()) {
-      const fileContent = await fs.readFile(svgFile, 'utf8');
+      const name = file.substr(0, file.lastIndexOf('.'));
 
-      const m = regex.exec(fileContent);
-
-      if (m != null) {
-        const name = file.substr(0, file.lastIndexOf('.'));
-        const height = m[1];
-        const width = m[2];
-        const fill = m[3];
-        const viewBox = m[4];
-        const svgContent = m[5];
-
-        // building js file content
-        let content = '';
-
-        content += `export default {\n  name: '${name}',\n  height: ${height},\n  width: ${width},\n  svgContent: '${svgContent}',\n fill: '${fill}',\n viewBox: '${viewBox}'\n};\n`;
-
-        // do some sanity checks before writing
-        if (!name) {
-          console.error(`${svgFile}: File Name doesn't match expected format. Expects {svgName}.svg`);
-        } else if (!height || isNaN(height)
-                    || !width || isNaN(width)
-                    || !svgContent) {
-          console.error(`${svgFile}: Content not in expected format! Executed regex didn't deliver results.`);
-        } else {
-          fs.writeFile(jsFile, content);
-          addedIcons.push(file.replace('.svg', ''));
-        }
+      // do some sanity checks before writing
+      if (!name) {
+        console.error(`${svgFile}: File Name doesn't match expected format. Expects {svgName}.svg`);
       } else {
-        console.error(`${svgFile}: Content not found! Executed regex didn't deliver results.`);
+        addedIcons.push(file.replace('.svg', ''));
       }
     } else if (stat.isDirectory()) {
       console.error("'%s' is a directory. This script is currently not recursive and can't handle sub directories.", svgFile);
     }
   }
 
-  const makeCamelCase = (str) => str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr);
+  const makeCamelCase = (str) => str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
 
-  const indexFileImportContent = `${addedIcons.map((addedIcon) => `import ${makeCamelCase(addedIcon)} from './${addedIcon}'`).join(';\n')};\n\n`;
+  const svgImports = addedIcons.map((addedIcon) => 
+  `import ${makeCamelCase(addedIcon)}Icon from ".${svgSourceFolder}${addedIcon}.svg"`).join(';\n') + ";"
+ 
+  const indexFileRegistryContent = "\n export const iconRegistry = {};"+
+  "\n" + addedIcons.map((addedIcon) => `export const ${makeCamelCase(addedIcon)} = () => iconRegistry["${makeCamelCase(addedIcon)}"] = ${makeCamelCase(addedIcon)}Icon`).join(';\n') + ";"
 
-  const indexFileExportIconsObject = `const icons = {\n${
-    addedIcons.map((addedIcon) => `  ${makeCamelCase(addedIcon)}`).join(',\n')},\n};\n`;
 
-  const indexFileExportGetterFunction = `\n export const getIcon = (icon) => icons[icon]`;
-
-  const data = [indexFileImportContent, indexFileExportIconsObject, indexFileExportGetterFunction];
+  const data = [svgImports, indexFileRegistryContent];
 
   fs.writeFile(`${jsTargetFolder}index.js`, data, (err3) => {
     if (err3) {
